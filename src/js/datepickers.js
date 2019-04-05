bd0a176324168b51848fb937b856f8c751c4cb93
Artem Safin <artemsafin.webdev@gmail.com>
2019-04-05 13:49:44 +0300

    Испарвлен баг с датапикером

--- src/js/datepickers.js
+++ src/js/datepickers.js
@@ -4,35 +4,30 @@ import moment from 'moment';
 
 
 export default function() {
+  const fromInputElement = document.getElementById('from')
+  const toInputElement = document.getElementById('to')
+
+  if (fromInputElement) {
+    const fromValue = fromInputElement.value
+    $(fromInputElement).datepicker({
+      position: "bottom right",
+    })
+    if (fromValue) {
+      $(fromInputElement).data('datepicker').selectDate(moment(fromValue, 'DD.MM.YYYY').toDate())
+    }
+  }
+
+  if (toInputElement) {
+    const toValue = toInputElement.value
+    $(toInputElement).datepicker({
+      position: "bottom right",
+      maxDate: new Date()
+    })
+    if (toValue) {
+      $(toInputElement).data('datepicker').selectDate(moment(toValue, 'DD.MM.YYYY').toDate())
+    }
+  }
  
-
-  const from = $('#from')
-  const to = $('#to')
-  const fromValue = document.getElementById('from').value
-  const toValue = document.getElementById('to').value
-
-  from.datepicker({
-    position: "bottom right",
-    // startDate: fromValue ? moment(fromValue, 'DD.MM.YYYY').toDate() : new Date()
-  })
-
-  if (fromValue) {
-    from.data('datepicker').selectDate(moment(fromValue, 'DD.MM.YYYY').toDate())
-  }
-  
-  
-  to.datepicker({
-    position: "bottom right",
-    maxDate: new Date()
-  })
-
-  if (toValue) {
-    to.data('datepicker').selectDate(moment(toValue, 'DD.MM.YYYY'))
-  }
-
-  
-
-
   $('.js-birthday-input').datepicker({
     maxDate: new Date()
   })
