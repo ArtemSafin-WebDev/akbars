8a3f628a16425d03dc7e2884ce23fe606535e34b
Artem Safin <artemsafin.webdev@gmail.com>
2019-04-05 11:09:26 +0300

    Поправлен выбор дат в датапикере в Новостях, изменена привязка стилей к классам

--- src/js/datepickers.js
+++ src/js/datepickers.js
@@ -1,17 +1,39 @@
 
 import 'air-datepicker';
+import moment from 'moment';
 
 
 export default function() {
-  $('#from').datepicker({
-    position: "bottom right"
+ 
+
+  const from = $('#from')
+  const to = $('#to')
+  const fromValue = document.getElementById('from').value
+  const toValue = document.getElementById('to').value
+
+  from.datepicker({
+    position: "bottom right",
+    // startDate: fromValue ? moment(fromValue, 'DD.MM.YYYY').toDate() : new Date()
   })
-  $('#to').datepicker({
+
+  if (fromValue) {
+    from.data('datepicker').selectDate(moment(fromValue, 'DD.MM.YYYY').toDate())
+  }
+  
+  
+  to.datepicker({
     position: "bottom right",
     maxDate: new Date()
   })
+
+  if (toValue) {
+    to.data('datepicker').selectDate(moment(toValue, 'DD.MM.YYYY'))
+  }
+
+  
+
+
   $('.js-birthday-input').datepicker({
-    
     maxDate: new Date()
   })
 }
