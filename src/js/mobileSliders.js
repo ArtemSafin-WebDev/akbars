fcaa4bd995774db08fb87d154d1a4fa096d9b5ff
Artem Safin <artemsafin.webdev@gmail.com>
2019-04-09 10:05:57 +0300

    Все блоки мобильной версии

--- src/js/mobileSliders.js
+++ src/js/mobileSliders.js
@@ -29,4 +29,44 @@ export default function() {
     slidesPerView: 'auto',
     spaceBetween: 15
   })
+
+  let regularNameSlider = new Swiper('.js-swiper-regular-name-slider', {
+    loop: true,
+    navigation: {
+      nextEl: '.js-swiper-regular-next',
+      prevEl: '.js-swiper-regular-prev'
+    }
+  })
+
+  let regularTableSlider = new Swiper('.js-swiper-regular-table', {
+    loop: true
+  })
+
+  if (regularNameSlider && regularTableSlider) {
+    regularNameSlider.controller.control = regularTableSlider
+    regularTableSlider.controller.control = regularNameSlider
+  }
+
+  let playoffNameSlider = new Swiper('.js-swiper-playoff-name-slider', {
+    loop: true,
+    navigation: {
+      nextEl: '.js-swiper-playoff-next',
+      prevEl: '.js-swiper-playoff-prev'
+    }
+  })
+
+
+  let playoffTableSlider = new Swiper('.js-swiper-playoff-table', {
+    loop: true
+  })
+
+
+  if (playoffNameSlider && playoffTableSlider) {
+    playoffNameSlider.controller.control = playoffTableSlider
+    playoffTableSlider.controller.control = playoffNameSlider
+  }
+  
+
+
+
 }
