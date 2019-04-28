ba995bd5d98709a0c4cc9787667751e6919c00a8
Artem Safin <artemsafin.webdev@gmail.com>
2019-04-28 13:25:25 +0300

    Немного изменена страница настроек профиля, регистрации и добавлена страница смены пароля

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -2,42 +2,56 @@ import 'parsleyjs'
 import moment from 'moment'
 
 export default function() {
-  window.Parsley.addValidator("moment", {
-    requirementType: "string",
+  window.Parsley.addValidator('moment', {
+    requirementType: 'string',
     validateString: function(value) {
-      return moment(value, 'D.M.YYYY', true).isValid();
+      return moment(value, 'D.M.YYYY', true).isValid()
     },
     messages: {
-      en: "Please enter a valid date.",
-      ru: "Укажите правильную дату в формате дд.мм.гггг"
+      en: 'Please enter a valid date.',
+      ru: 'Укажите правильную дату в формате дд.мм.гггг'
     }
-  });
+  })
 
 
-  Parsley.addMessages("ru", {
-    defaultMessage: "Некорректное значение.",
-    type: {
-      email: "В данном поле может быть только E-mail",
-      url: "Введите URL адрес.",
-      number: "Введите число.",
-      integer: "Введите целое число.",
-      digits: "Введите только цифры.",
-      alphanum: "Введите буквенно-цифровое значение."
+  window.Parsley.addValidator('passformat', {
+    requirementType: 'string',
+    validateString: function(value) {
+      console.log(value)
+      var re = /[^0-9A-Za-z\!\@\#\$\%\^\&\*\(\)\{\}\[\]\_\-\+\=\|\.\,]/u;
+      return !re.test(value)
     },
-    notblank: "Это поле должно быть заполнено.",
-    required: "Обязательное поле",
-    pattern: "Это значение некорректно.",
-    min: "Это значение должно быть не менее чем %s.",
-    max: "Это значение должно быть не более чем %s.",
-    range: "Это значение должно быть от %s до %s.",
-    minlength: "Это значение должно содержать не менее %s символов.",
-    maxlength: "Это значение должно содержать не более %s символов.",
-    length: "Это значение должно содержать от %s до %s символов.",
-    mincheck: "Выберите не менее %s значений.",
-    maxcheck: "Выберите не более %s значений.",
-    check: "Выберите от %s до %s значений.",
-    equalto: "Это значение должно совпадать."
-  });
+    messages: {
+      en: 'Password does not meet the requirements',
+      ru: 'Пароль не соответствует требованиям по символам'
+    }
+  })
 
-  Parsley.setLocale("ru");
+
+  Parsley.addMessages('ru', {
+    defaultMessage: 'Некорректное значение.',
+    type: {
+      email: 'В данном поле может быть только E-mail',
+      url: 'Введите URL адрес.',
+      number: 'Введите число.',
+      integer: 'Введите целое число.',
+      digits: 'Введите только цифры.',
+      alphanum: 'Введите буквенно-цифровое значение.'
+    },
+    notblank: 'Это поле должно быть заполнено.',
+    required: 'Обязательное поле',
+    pattern: 'Это значение некорректно.',
+    min: 'Это значение должно быть не менее чем %s.',
+    max: 'Это значение должно быть не более чем %s.',
+    range: 'Это значение должно быть от %s до %s.',
+    minlength: 'Это значение должно содержать не менее %s символов.',
+    maxlength: 'Это значение должно содержать не более %s символов.',
+    length: 'Это значение должно содержать от %s до %s символов.',
+    mincheck: 'Выберите не менее %s значений.',
+    maxcheck: 'Выберите не более %s значений.',
+    check: 'Выберите от %s до %s значений.',
+    equalto: 'Это значение должно совпадать.'
+  })
+
+  Parsley.setLocale('ru')
 }
